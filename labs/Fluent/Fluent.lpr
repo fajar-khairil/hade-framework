2caf86ec1b45c3c589b2352044b337b4eb6585df
kodok-buncit <fajar90alone@gmail.com>
2014-05-11 13:48:14 +0700

    initial implementation of Fluent

--- labs/Fluent/Fluent.lpr
+++ labs/Fluent/Fluent.lpr
@@ -6,9 +6,28 @@ uses
   {$IFDEF UNIX}{$IFDEF UseCThreads}
   cthreads,
   {$ENDIF}{$ENDIF}
-  Classes
+  Classes,
+  sysutils,
+  hade.Fluent
   { you can add units after this };
 
+var
+  obj : TFluent;
+  lDate: TDateTime;
 begin
+  lDate := sysutils.StrToDateTime('1990-03-02 11:12:03',FluentFormatSetting);
+  obj := TFluent.Create( lDate );
+  try
+    WriteLn(obj.AsString);
+    WriteLn(obj.AsDate);
+    WriteLn(obj.AsDateTime);
+    WriteLn(obj.AsTime);
+    WriteLn(obj.AsDouble);
+    WriteLn(obj.AsInteger);
+    WriteLn(obj.AsUInteger);
+    WriteLn(obj.Value);
+  finally
+    obj.Free;
+  end;
 end.
 
--- src/Foundation/hade.fluent.pas
+++ src/Foundation/hade.fluent.pas
@@ -1,5 +1,12 @@
 unit hade.Fluent;
-
+{******************************************************************************
+ * This File is Part of HadeFramework Project
+ *
+ * Copyright (C) Fajar Khairil
+ * License MPL 1.1
+ *
+ * Description : Fluent Object Interface
+ ******************************************************************************}
 {$mode objfpc}{$H+}
 
 interface
@@ -32,6 +39,8 @@ Type
     procedure setString(AValue: string);
     procedure setTime(AValue: TTime);
     procedure setUInteger(AValue: ptrUint);
+
+    procedure RaiseError(const AMsg : String);
   public
     property AsString: string read getString write setString;
     property AsInteger : ptrInt read getInteger write setInteger;
@@ -40,95 +49,155 @@ Type
     property AsDateTime : TDateTime read getDateTime write setDateTime;
     property AsDate : TDate read getDate write setDate;
     property AsTime : TTime read getTime write setTime;
+    property Value : Variant read FValue write FValue;
+
+    Constructor Create(const AValue : Variant);
   end;
 
+var
+  FluentDefaultFormatSettings : TFormatSettings = (
+    CurrencyFormat: 1;
+    NegCurrFormat: 5;
+    ThousandSeparator: ',';
+    DecimalSeparator: '.';
+    CurrencyDecimals: 2;
+    DateSeparator: '-';
+    TimeSeparator: ':';
+    ListSeparator: ',';
+    CurrencyString: '$';
+    ShortDateFormat: 'yyyy-mm-dd';
+    LongDateFormat: 'yyyy" "mmmm" "dd';
+    TimeAMString: 'AM';
+    TimePMString: 'PM';
+    ShortTimeFormat: 'hh:nn';
+    LongTimeFormat: 'hh:nn:ss';
+    ShortMonthNames: ('Jan','Feb','Mar','Apr','May','Jun',
+                      'Jul','Aug','Sep','Oct','Nov','Dec');
+    LongMonthNames: ('January','February','March','April','May','June',
+                     'July','August','September','October','November','December');
+    ShortDayNames: ('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
+    LongDayNames:  ('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+    TwoDigitYearCenturyWindow: 50;
+  );
+
+  FluentFormatSetting : TFormatSettings absolute FluentDefaultFormatSettings;
+
 implementation
+const
+  CEConvertError = 'Unable to convert value to %s.';
 
 { TFluent }
 
 function TFluent.getDate: TDate;
 begin
   if variants.VarIsFloat(FValue) then
-    Result := variants.VarToDateTime(FValue)
+    Result := StrToDate(self.getString,FluentFormatSetting)
   else
-    raise EFluent.Create('Unable to Convert value to Double');
+    Result := getDouble;
 end;
 
 function TFluent.getDateTime: TDateTime;
 begin
   if variants.VarIsFloat(FValue) then
-    Result := self.getDate
+    Result := StrToDateTime(self.getString,FluentFormatSetting)
   else
-    raise EFluent.Create('Unable to Convert value to Double');
+    Result := getDouble;
 end;
 
 function TFluent.getDouble: Double;
 begin
-  if variants.VarIsFloat(FValue) then
+  if variants.VarIsNumeric(FValue) then
     Result := Double(FValue)
+  else if variants.VarType(FValue) = vardate then
+    Result := StrToDateTime(self.getString,FluentFormatSetting)
+  else if variants.VarIsStr(FValue) then
+    Result := StrToFloatDef(FValue,0)
   else
-    raise EFluent.Create('Unable to Convert value to Double');
+    RaiseError(Format(CEConvertError,['Double']));
 end;
 
 function TFluent.getInteger: ptrInt;
 begin
-  if variants.VarIsType(FValue) then
+  if (variants.VarIsNumeric(FValue)) OR (variants.VarType(FValue) = vardate) then
     Result := ptrInt(FValue)
+  else if Variants.VarIsStr(FValue)  then
+    if not TryStrToInt(variants.VarToStr(FValue),Result) then
+      RaiseError(Format(CEConvertError,['Integer']))
+    else
+      exit
   else
-    raise EFluent.Create('Unable to Convert value to Double');
+    RaiseError(Format(CEConvertError,['Integer']));
 end;
 
 function TFluent.getString: string;
 begin
-  if variants.VarIsFloat(FValue) then
-    Result := Double(FValie)
+  if variants.VarType(FValue) = vardate then
+    Result := DateTimeToStr(FValue,FluentFormatSetting)
   else
-    raise EFluent.Create('Unable to Convert value to Double');
+    Result := variants.VarToStr(FValue);
 end;
 
 function TFluent.getTime: TTime;
 begin
-
+  if variants.VarIsFloat(FValue) then
+    Result := StrToTime(self.getString,FluentFormatSetting)
+  else
+    Result := getDouble;
 end;
 
 function TFluent.getUInteger: ptrUint;
 begin
-
+  if (variants.VarIsNumeric(FValue)) OR (variants.VarType(FValue) = vardate) then
+    Result := ptrUint(FValue)
+  else if variants.VarIsStr(FValue) then
+    Result := StrToInt(FValue)
+  else
+    RaiseError(Format(CEConvertError,['Unsigned Integer']));
 end;
 
 procedure TFluent.setDate(AValue: TDate);
 begin
-
+  FValue := AValue;
 end;
 
 procedure TFluent.setDateTime(AValue: TDateTime);
 begin
-
+ FValue := AValue;
 end;
 
 procedure TFluent.setDouble(AValue: Double);
 begin
-
+ FValue := AValue;
 end;
 
 procedure TFluent.setInteger(AValue: ptrInt);
 begin
-
+ FValue := AValue;
 end;
 
 procedure TFluent.setString(AValue: string);
 begin
-
+ FValue := AValue;
 end;
 
 procedure TFluent.setTime(AValue: TTime);
 begin
-
+ FValue := AValue;
 end;
 
 procedure TFluent.setUInteger(AValue: ptrUint);
 begin
+ FValue := AValue;
+end;
 
+procedure TFluent.RaiseError(const AMsg: String);
+begin
+  raise EFluent.Create(AMsg);
+end;
+
+constructor TFluent.Create(const AValue: Variant);
+begin
+  FValue := AValue;
 end;
 
 end.
